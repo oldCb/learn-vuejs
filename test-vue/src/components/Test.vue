b8f0348f41b740a122eaacd80dcf2dc588eff446
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-04 11:19:16 +0200

    add new components

---
+++ test-vue/src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="myComponent">
+    <h1>Nouveau composant</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .myComponent {
+    text-align: center;
+    background-color: #008000;
+    color: #ffffff;
+  }
+</style>
+
+
--- test-vue/src/views/About.vue
+++ test-vue/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <test />
   </div>
 </template>
+
+<script>
+import Test from '@/components/Test.vue'
+export default {
+  name: 'About',
+
+  components: {
+    Test
+  }
+}
+</script>
+
