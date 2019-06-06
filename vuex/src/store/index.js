31fadc529cc11eb3f4411c1ab0d4df337627e104
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-06 10:19:07 +0200

    new store

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import compteur from './modules/compteur'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    compteur
   }
 })
---
+++ vuex/src/store/modules/compteur.js
@@ -0,0 +1,19 @@
+// import mutation-types
+
+const state = {}
+
+// methodes de lancement des mutations
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
