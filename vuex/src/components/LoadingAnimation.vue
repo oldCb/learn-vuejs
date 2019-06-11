cc75f76caa54008b0d7331986c9ce92fe1199c39
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-11 10:25:48 +0200

    color loading animation

--- vuex/src/components/LoadingAnimation.vue
+++ vuex/src/components/LoadingAnimation.vue
@@ -19,7 +19,7 @@
   width: 0.8em;
   height: 0.8em;
   border-radius: 50%;
-  background-color: #fc2f70;
+  background-color: #fc2f40;
   transform: translateY(-100%);
   animation: wave 0.8s ease-in-out alternate infinite;
 }
